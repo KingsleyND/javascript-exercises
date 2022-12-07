8547b8734813a7cffa21c1ac0d6dea38792db5f1
Kingsley <72080248+KingsleyND@users.noreply.github.com>
2022-12-07 12:27:19 +0100

    finished exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -3,10 +3,17 @@ const sumAll = function(num1,num2) {
     if (typeof(num1) != "number" ||typeof(num2) != "number"|| num1<1 || num2<1 ){
     return "ERROR"
   }
+  if (num1 > num2){
+      for(i = num2; i<=num1; i++){
+      final_sum +=i;
+    }
+    return final_sum;
+  }else{
     for(i = num1; i<=num2; i++){
       final_sum +=i;
     }
-    return final_sum
+    return final_sum;
+  }
   };
   
   
