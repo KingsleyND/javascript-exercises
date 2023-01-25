71e339254ecc025be122fbc0b97ba93e7504ed38
Kingsley <72080248+KingsleyND@users.noreply.github.com>
2023-01-25 12:47:21 +0100

    added a cleaner way of solving

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,14 @@
-const getTheTitles = function() {
+const getTheTitles = function(books) {
+//     titles = []
 
+//    for(let i=0; i<books.length; i++){
+//      titles.push(books[i].title)
+//    }
+//return titles
+return array.map(book => book.title);
+  
 };
+
 
 // Do not edit below this line
 module.exports = getTheTitles;
