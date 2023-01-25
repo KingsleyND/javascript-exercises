d2d9878e6b5eab4ff8a569d940d4d3b809879187
Kingsley <72080248+KingsleyND@users.noreply.github.com>
2023-01-25 12:20:14 +0100

    finished fibonacci exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,15 @@
-const fibonacci = function() {
-
+const fibonacci = function(position) {
+    if(position<0){return "OOPS"}
+    sequence = [1,1]
+    for(let i = 0; i < position; i++){
+      fibonacciNumber = sequence[sequence.length -1]+sequence[sequence.length -2]
+      sequence.push(fibonacciNumber)
+    }
+   return sequence[position-1];
 };
+
+
+
 
 // Do not edit below this line
 module.exports = fibonacci;
