39eda9d838f5fc82f830419425ba537b31b71cff
Kingsley <72080248+KingsleyND@users.noreply.github.com>
2023-01-25 10:39:06 +0100

    finished exercise 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,19 @@
-const palindromes = function () {
-
+const palindromes = function (word) {
+  const processedWord = word.toLowerCase().replace(/[,!$-. ]/g, "");
+    let wordLength = processedWord.length
+    
+    reverseWord = ""
+    for (i = 0; i < word.length; i++){
+      wordLength -=1
+ 
+     reverseWord += processedWord.charAt(wordLength)
+    };
+  
+  return (processedWord === reverseWord)?true:false;
+  
 };
+
+
 
 // Do not edit below this line
 module.exports = palindromes;
