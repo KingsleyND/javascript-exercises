8d271131369704ac51df73b3cedae1d7aebff057
Kingsley <72080248+KingsleyND@users.noreply.github.com>
2023-01-30 12:11:11 +0100

    finished Find the oldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,23 @@
-const findTheOldest = function() {
+const findTheOldest = function(peopleArray) {
+    
+        return peopleArray.reduce((a,b)=>{
+          const oldestAge = getAge(a.yearOfDeath, a.yearOfBirth)
+          const currentAge = getAge(b.yearOfDeath, b.yearOfBirth)
+    
+          return oldestAge > currentAge ? a:b;
+        })
+     };
+     
+     function getAge(death,birth){
+        if(!death){
+            death = new Date().getFullYear()
+        }
+       return death-birth;
 
-};
+     } ;   
+
+
+ 
 
 // Do not edit below this line
 module.exports = findTheOldest;
